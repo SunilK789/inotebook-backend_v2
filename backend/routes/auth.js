f386aaa1d3ca92f7484046fb02a2cc896fd7c106
Sunil Kumar <sunilpalarya@gmail.com>
2023-02-11 11:39:15 +0530

    after login token success

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,6 +50,55 @@ router.post(
 			res.json({authToken})
 		} catch (error) {
 			console.error(error.message)
+			res.status(500).send("Internal server error")
+		}
+	}
+)
+
+//user login using: POST "/api/auth/login". email and password required
+router.post(
+	"/login",
+	[
+		body("email", "Enter a valid email").isEmail(),
+		body("password", "Password must be atleast 5 character").exists(),
+	],
+	async (req, res) => {
+		//If there are errors return bad request
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({errors: errors.array()})
+		}
+
+		//check wheather the user exist with same email id
+		try {
+			let user = await User.findOne({email: req.body.email})
+			if (!user) {
+				return res
+					.status(400)
+					.json({error: "Please try to enter correct credencials!"})
+			}
+
+			const passwordComapre = await bcrypt.compare(
+				req.body.password,
+				user.password
+			)
+			if (!passwordComapre) {
+				return res
+					.status(400)
+					.json({error: "Please try to enter correct credencials!"})
+			}
+
+			const data = {
+				user: {
+					id: user.id,
+				},
+			}
+			const authToken = jwt.sign(data, JWT_SECRET)
+			console.log(authToken)
+
+			res.json({authToken})
+		} catch (error) {
+			console.error(error.message)
 			res.status(500).send("Some error occured!")
 		}
 	}
